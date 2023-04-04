b046c588d53db9bfc08b23217f46e12a9ffbb367
vcnovaes <vcn2@cin.ufpe.br>
2023-04-04 02:06:50 -0300

    Adding search term support

---
+++ src/Movies/MoviesService.ts
@@ -0,0 +1,40 @@
+import { Service } from "../Common/Service";
+import { DatabaseClient } from "../Database/DatabaseClient";
+import { DatabaseInternalError } from "../Database/Exceptions";
+import { Movie } from "../types/movies.type";
+
+export class MoviesService extends Service {
+    defaultPageSize = 10
+    constructor() {
+        super("movie", new DatabaseClient())
+    }
+
+    public getMovies(confirmedEmail: boolean = false, offset = 0, pageSize = 0, searchTerm = "") {
+        if (pageSize == 0) pageSize = this.defaultPageSize
+        if (searchTerm.length === 0)
+            return new Promise<Movie[]>((resolve, reject) => {
+                this.dbClient.all(`SELECT id, name, description, rating FROM movie WHERE secret=(?) OR secret=0 LIMIT (?) OFFSET (?)`,
+                    [confirmedEmail, pageSize, offset],
+                    (error, data) => {
+                        if (error) {
+                            reject(new DatabaseInternalError("Not possible to get data about movies"))
+                        }
+                        resolve((data as Movie[]))
+                    }
+                )
+            })
+
+        return new Promise<Movie[]>((resolve, reject) => {
+            const query = `SELECT id, name, description, rating FROM movie WHERE (secret=(?)  OR secret=0)  AND (name LIKE "%${searchTerm}%"  OR  description LIKE "%${searchTerm}%") LIMIT (?) OFFSET (?)`
+            this.dbClient.all(query,
+                [confirmedEmail, pageSize, offset],
+                (error, data) => {
+                    if (error) {
+                        reject(new DatabaseInternalError("Not possible to get data about movies"))
+                    }
+                    resolve((data as Movie[]))
+                }
+            )
+        })
+    }
+}
