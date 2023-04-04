ea7bb31d1aa7652f755bf6824738a8332de94cbe
vcnovaes <vcn2@cin.ufpe.br>
2023-04-04 01:08:06 -0300

    blocklist singleton

---
+++ src/Auth/Blocklist.ts
@@ -0,0 +1,32 @@
+import { Queue } from "queue-typescript"
+
+
+// singleton
+export default class Blocklist {
+    private list;
+    private order;
+    private static blocklist: Blocklist;
+    private constructor() {
+        this.list = new Set<string>();
+        this.order = new Queue<string>();
+    }
+    public static createBlocklist() {
+        if (!Blocklist.blocklist) {
+            Blocklist.blocklist = new Blocklist()
+        }
+        return Blocklist.blocklist;
+    }
+
+    has(token: string) {
+        return this.list.has(token)
+    }
+    removeLast() {
+        const lastElement = this.order.tail;
+        this.list.delete(lastElement)
+        this.order.dequeue()
+    }
+    add(token: string) {
+        this.list.add(token)
+        this.order.enqueue(token)
+    }
+}
