0b0a1cc93e4282eef7b6445b17e91ddfbd468888
vcnovaes <vcn2@cin.ufpe.br>
2023-04-04 01:09:28 -0300

    server with authentication

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+
+import { Router, Request, Response } from 'express';
+import { UserRouter } from './User';
+
+import { config } from 'dotenv';
+import { authMiddleware } from './Auth';
+import { ItemsRouter } from './Items/ItemsRouter';
+import Blocklist from './Auth/Blocklist';
+const app = express();
+
+export const blocklist = Blocklist.createBlocklist()
+
+config()
+
+app.use(express.json())
+
+const router = express.Router()
+app.use("/", UserRouter)
+
+app.use("/auth/", authMiddleware, ItemsRouter)
+app.listen(3333, () => 'server running on port 3333')
