7acea5c088e6ed754e73a824567f401cf10d6c11
vcnovaes <vcn2@cin.ufpe.br>
2023-04-04 02:42:38 -0300

    Renaming folder

--- src/Items/ItemsRouter.ts
+++
@@ -1,3 +0,0 @@
-import { Router } from "express";
-
-export const ItemsRouter = Router().get("/hi", (req, res) => res.send("Hello"))
--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,15 @@ import { UserRouter } from './User';
 
 import { config } from 'dotenv';
 import { authMiddleware } from './Auth';
-import { ItemsRouter } from './Items/ItemsRouter';
+import { MoviesRouter } from './Movies/MoviesRouter';
 import Blocklist from './Auth/Blocklist';
+import ActiveList from './Auth/Active';
 const app = express();
 
 export const blocklist = Blocklist.createBlocklist()
+export const activeList = ActiveList.create()
 
-setTimeout(() => blocklist.removeLast(), (1000) * 60 * 60)
+setTimeout(() => { blocklist.removeLast(); activeList.removeLast() }, (1000) * 60 * 60)
 
 
 config()
@@ -21,5 +23,5 @@ app.use(express.json())
 const router = express.Router()
 app.use("/", UserRouter)
 
-app.use("/auth/", authMiddleware, ItemsRouter)
+app.use("/auth/", authMiddleware, MoviesRouter)
 app.listen(3333, () => 'server running on port 3333')
