6b6ab4347520b4dadcf7c8f267b9ad1c2e32af08
vcnovaes <vcn2@cin.ufpe.br>
2023-04-04 01:05:59 -0300

    User service

---
+++ src/User/UserService.ts
@@ -0,0 +1,64 @@
+import { comparePassword, hasher } from "../Auth/Auth";
+import { DatabaseClient } from "../Database/DatabaseClient";
+import { DatabaseInternalError, ObjectNotFound } from "../Database/Exceptions";
+import { User } from "../types";
+
+export class UserService {
+
+    dbClient: DatabaseClient
+    tableName = "user"
+    primaryKey = "username"
+    constructor() {
+        this.dbClient = new DatabaseClient()
+    }
+    private async getUser(username: string) {
+        try {
+            const user: User = await this.dbClient.getObject(this.tableName, this.primaryKey, username)
+            if (!user) {
+                throw new ObjectNotFound(`Not possible to found username  ${username}`)
+            }
+            return user;
+        } catch (error) {
+            console.error(error)
+            throw error
+        } finally {
+            this.dbClient.close()
+        }
+    }
+
+    public async login(username: string, password: string) {
+        try {
+            const user = await this.getUser(username)
+            if (comparePassword(password, user.password)) {
+                throw new ObjectNotFound("Username or password are incorrect")
+            }
+            return user
+        } catch (error) {
+            console.error("Error in login on UserService:", error)
+            throw error
+        }
+    }
+
+    public async register(user: User) {
+        try {
+            const query = `INSERT INTO user (username, email, first_name, last_name, profile_img, password) VALUES (?,?,?,?,?,?)`;
+            return new Promise<boolean>((resolve, reject) => {
+                this.dbClient.run(query, [user.username, user.email, user.firstName, user.lastName, user.profileImage, hasher(user.password)],
+                    (error) => {
+                        if (error !== null) {
+                            console.error("Not possible to register user: ", user.username)
+                            reject(false)
+                            throw new DatabaseInternalError(` Not possible to register: ${error.message}`)
+                        }
+                        resolve(true)
+                    }
+                )
+            })
+        } catch (error) {
+            console.error(error)
+            throw error;
+        }
+    }
+}
+
+
